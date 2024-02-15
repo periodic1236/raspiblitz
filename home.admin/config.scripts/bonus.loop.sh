d00f62f84b46f0bdb448ef23131d0fd0ccd2ccf9
openoms <oms@tuta.io>
2024-02-15 10:00:18 +0100

    loop update to v0.27.0-beta

--- home.admin/config.scripts/bonus.loop.sh
+++ home.admin/config.scripts/bonus.loop.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# NOTICE: Faraday is now part of the 'bonus.lit.sh' bundle
+# NOTICE: Loop is now part of the 'bonus.lit.sh' bundle
 # this single install script will still be available for now
 # but main focus for the future development should be on LIT
 
-# https://github.com/lightninglabs/loop/releases-
-pinnedVersion="v0.15.0-beta"
+# https://github.com/lightninglabs/loop/releases
+pinnedVersion="v0.27.0-beta"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
- echo "config script to switch the Lightning Loop Service on,off or update"
- echo "bonus.loop.sh [on|off|menu|update]"
- echo "# DEPRECATED use instead: bonus.lit.sh"
- exit 1
+  echo "config script to switch the Lightning Loop Service on,off or update"
+  echo "bonus.loop.sh [on|off|menu|update]"
+  echo "# DEPRECATED use instead: bonus.lit.sh"
+  exit 1
 fi
 
 # show info menu
@@ -28,12 +28,14 @@ fi
 # releases are creatd on GitHub
 PGPsigner="web-flow"
 PGPpubkeyLink="https://github.com/${PGPsigner}.gpg"
-PGPpubkeyFingerprint="4AEE18F83AFDEB23"
+PGPpubkeyFingerprint="B5690EEEBB952194"
 
 # TODO download with .tar.gz
 #PGPsigner="alexbosworth"
 #PGPpubkeyLink="https://github.com/${PGPsigner}.gpg"
 #PGPpubkeyFingerprint="E80D2F3F311FD87E"
+
+source /mnt/hdd/raspiblitz.conf
 
 # stop services
 echo "making sure the loopd.service is not running"
@@ -42,24 +44,27 @@ sudo systemctl stop loopd 2>/dev/null
 # switch on
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo "# Install Lightning Loop"
-  
+
   isInstalled=$(sudo ls /etc/systemd/system/loopd.service 2>/dev/null | grep -c 'loopd.service')
   if [ ${isInstalled} -eq 0 ]; then
 
     # install Go
     /home/admin/config.scripts/bonus.go.sh on
-    
+
     # get Go vars
     source /etc/profile
 
-    # create dedicated user
-    sudo adduser --system --group --home /home/loop loop
+  USERNAME=loop
+  echo "# add the user: ${USERNAME}"
+  sudo adduser --system --group --shell /bin/bash --home /home/${USERNAME} ${USERNAME}
+  echo "Copy the skeleton files for login"
+  sudo -u ${USERNAME} cp -r /etc/skel/. /home/${USERNAME}/
 
     # set PATH for the user
     sudo bash -c "echo 'PATH=\$PATH:/home/loop/go/bin/' >> /home/loop/.profile"
 
     # make sure symlink to central app-data directory exists
-    sudo rm -rf /home/loop/.lnd  # not a symlink.. delete it silently
+    sudo rm -rf /home/loop/.lnd # not a symlink.. delete it silently
     # create symlink
     sudo ln -s /mnt/hdd/app-data/lnd/ /home/loop/.lnd
 
@@ -79,7 +84,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     cd /home/loop/loop
     sudo -u loop git reset --hard $pinnedversion
     sudo -u loop /home/admin/config.scripts/blitz.git-verify.sh \
-     "${PGPsigner}" "${PGPpubkeyLink}" "${PGPpubkeyFingerprint}" || exit 1
+      "${PGPsigner}" "${PGPpubkeyLink}" "${PGPpubkeyFingerprint}" || exit 1
     cd /home/loop/loop/cmd
     sudo -u loop /usr/local/go/bin/go install ./... || exit 1
 
@@ -109,7 +114,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
       proxy=""
     fi
 
-    # sudo nano /etc/systemd/system/loopd.service 
+    # sudo nano /etc/systemd/system/loopd.service
     echo "
 [Unit]
 Description=Loopd Service
@@ -137,7 +142,7 @@ WantedBy=multi-user.target
     sudo systemctl enable loopd
     echo "# OK - the Lightning Loop service is now enabled"
 
-  else 
+  else
     echo "# The Loop service already installed."
   fi
 
@@ -146,15 +151,15 @@ WantedBy=multi-user.target
 
   # setting value in raspi blitz config
   /home/admin/config.scripts/blitz.conf.sh set loop "on"
-  
+
   isInstalled=$(sudo -u loop /home/loop/go/bin/loop | grep -c loop)
-  if [ ${isInstalled} -gt 0 ] ; then
+  if [ ${isInstalled} -gt 0 ]; then
     echo "# Find info on how to use on https://github.com/lightninglabs/loop#loop-out-swaps"
   else
     echo "# Failed to install Lightning Loop "
     exit 1
   fi
-  
+
   exit 0
 fi
 
@@ -174,7 +179,7 @@ if [ "$1" = "0" ] || [ "$1" = "off" ]; then
     # delete user and it's home directory
     sudo userdel -rf loop
     echo "# OK, the Loop Service is removed."
-  else 
+  else
     echo "# Loop is not installed."
   fi
 
@@ -204,12 +209,12 @@ if [ "$1" = "update" ]; then
     TAG=$(git tag | sort -V | tail -1)
     sudo -u loop git reset --hard $TAG
     sudo -u loop /home/admin/config.scripts/blitz.git-verify.sh \
-     "${PGPsigner}" "${PGPpubkeyLink}" "${PGPpubkeyFingerprint}" || exit 1
+      "${PGPsigner}" "${PGPpubkeyLink}" "${PGPpubkeyFingerprint}" || exit 1
     echo "# Updating ..."
     # install to /home/loop/go/bin/
     cd /home/loop/loop/cmd
     sudo -u loop /usr/local/go/bin/go install ./... || exit 1
-    isInstalled=$(sudo -u loop /home/loop/go/bin/loop  | grep -c loop)
+    isInstalled=$(sudo -u loop /home/loop/go/bin/loop | grep -c loop)
     if [ ${isInstalled} -gt 0 ]; then
       TAG=$(git tag | sort -V | tail -1)
       echo "# Updated to version" $TAG
@@ -229,4 +234,3 @@ fi
 echo "# FAIL - Unknown Parameter $1"
 echo "# may need reboot to run normal again"
 exit 1
-  
